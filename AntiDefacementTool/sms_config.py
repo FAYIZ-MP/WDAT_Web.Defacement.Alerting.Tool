403ac2d97c9e4fee1e283e833e7f7a0809288213
FAYIZ M P <fayizmp2003@gmail.com>
2025-07-18 09:26:35 +0530

    Delete AntiDefacementTool directory

--- AntiDefacementTool/sms_config.py
+++
@@ -1,5 +0,0 @@
-TWILIO_ACCOUNT_SID = "twilio account sid"  # Your Twilio Account SID
-TWILIO_AUTH_TOKEN = "twilio auth token"  # Your Twilio Auth Token
-TWILIO_PHONE_NUMBER = "twilio phone number"  # Your Twilio phone number
-ADMIN_PHONE_NUMBER = "admin's phone number"  # The recipient of the alert SMS
-
